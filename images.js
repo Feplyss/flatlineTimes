daa0759a2b6ca96692569a25c8041ec7126b3d74
ADS 2021.1N <36124872021.1N@senacrjedu.local>
2022-10-24 19:51:34 -0300

    first commit

---
+++ images.js
@@ -0,0 +1,19 @@
+const images = {
+    '1': {
+        imgName: '1',
+        uri: require('./assets/flamengo.png')
+    },
+    '2': {
+        imgName: '2',
+        uri: require('./assets/vasco.png')
+    },
+    '3': {
+        imgName: '3',
+        uri: require('./assets/fluminense.png')
+    },
+    '4': {
+        imgName: '4',
+        uri: require('./assets/botafogo.png')
+    }
+}
+export { images };
